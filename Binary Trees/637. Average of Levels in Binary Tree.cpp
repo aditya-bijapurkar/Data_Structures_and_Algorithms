b26c985984793e2a25568a237e2ce8b6e43e6785
Aditya Bijapurkar <82723741+aditya-bijapurkar@users.noreply.github.com>
2022-09-03 14:01:12 +0530

    Update 637. Average of Levels in Binary Tree.cpp

--- Binary Trees/637. Average of Levels in Binary Tree.cpp
+++ Binary Trees/637. Average of Levels in Binary Tree.cpp
@@ -1,1 +1,27 @@
-v
+//  Link: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root){
+        vector<double> ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        long long curr;
+        
+        while(!q.empty()){
+            curr=0;
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                curr+=q.front()->val;
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+            ans.push_back((double)curr/s);
+        }
+        
+        return ans;
+    }
+};
