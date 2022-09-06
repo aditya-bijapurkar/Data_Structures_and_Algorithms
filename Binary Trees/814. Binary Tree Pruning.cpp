28e859fdc6d613eb39632813569b35fbb260f9a9
Aditya Bijapurkar <82723741+aditya-bijapurkar@users.noreply.github.com>
2022-09-07 01:24:33 +0530

    Create 814. Binary Tree Pruning.cpp

---
+++ Binary Trees/814. Binary Tree Pruning.cpp
@@ -0,0 +1,17 @@
+//  Link: https://leetcode.com/problems/binary-tree-pruning/
+
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {         
+        if(!root)
+            return NULL;
+        
+        root->left=pruneTree(root->left);
+        root->right=pruneTree(root->right);
+        
+        if(root->val==0 && !root->left && !root->right)
+            return NULL;
+        
+        return root;
+    }
+};
